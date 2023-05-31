1c0576cdca6b967508ec783096b1e5b4e82d502b
Technology-LaiNalson <nalson@chainsecurity.office.local>
2023-05-31 09:15:10 +0800

    nalson 2023/05/31

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', 'nuxt-icon', '@pinia/nuxt'],
+  typescript: {
+    typeCheck: true,
+  },
+  runtimeConfig: {
+    apiSecret: '怎麼可以讓你知道呢 :P',
+    public: {
+      apiBase: '/api',
+      googleClientId: '181599956887-2tp2frmm6thmnrvqa2apemba6ntcshd5.apps.googleusercontent.com',
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mx-6 mb-4 bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="text-6xl font-semibold text-gray-800">這裡是首頁</h1>
+      <div class="my-4 flex flex-col space-y-4">
+        <NuxtLink to="/posts">前往 Posts</NuxtLink>
+        <NuxtLink to="/about">前往 About</NuxtLink>
+        <NuxtLink to="/contact">前往 Contact</NuxtLink>
+        <NuxtLink to="/clientAndServer">前往 Client And Server</NuxtLink>
+        <NuxtLink to="/counter/increment">前往 /counter/increment</NuxtLink>
+        <NuxtLink to="/counter/surprise">前往 /counter/surprise</NuxtLink>
+        <NuxtLink to="/counter">前往 /counter</NuxtLink>
+        <NuxtLink to="/show">前往 /show</NuxtLink>
+        <NuxtLink to="/profile">前往 /profile</NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
