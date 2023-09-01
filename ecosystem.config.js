7b7ca02e8859a64b31a82d5063f51d8694241127
gahgah147 <gahgah147@gmail.com>
2023-09-01 05:44:02 +0000

    nalson

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,11 @@ module.exports = {
       name: 'NuxtAppName',
       exec_mode: 'cluster',
       instances: 'max',
-      script: './.output/server/index.mjs' 
+      script: './.output/server/index.mjs',
+      env: {
+        NITRO_PORT: 3001,
+        NITRO_HOST: '127.0.0.1'
+      }
     }
   ]
 }
